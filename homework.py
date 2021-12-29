5baaffeb581fc263652dbc1e670f1200f8cd460d
Alexey Chinenkov <alexer_ex@inbox.ru>
2021-12-29 19:44:52 +0300

    Fixed annotations

--- homework.py
+++ homework.py
@@ -153,7 +153,7 @@ def read_package(workout_type: str, data: list[float]) -> Training:
     return traning_types[workout_type](*data)
 
 
-def main(training: Type[Training]) -> None:
+def main(training: Training) -> None:
     """Главная функция."""
     info = training.show_training_info()
     print(info.get_message())
